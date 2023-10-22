94921460bba5dc609089ad3f78efb27bbb0d9395
filipe cavalcante <filipe.khalifa@gmail.com>
2023-10-22 16:05:43 -0300

    Subindo com swagger quebrado

---
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -0,0 +1,44 @@
+package dio.web.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dio.web.api.model.Usuario;
+import dio.web.api.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @PostMapping()
+    public void post(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+    @PutMapping()
+    public void put(@RequestBody Usuario usuario){
+        repository.update(usuario);
+    }
+    @GetMapping()
+    public List<Usuario> getAll(){
+        return repository.listAll();
+    }
+    @GetMapping("/{id}")
+    public Usuario getOne(@PathVariable("id") Integer id){
+        return repository.finById(id);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repository.remove(id);
+    }
+}
